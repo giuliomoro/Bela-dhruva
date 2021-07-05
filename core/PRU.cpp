d8e7f48cfc9769c5e7a1b59d6f770c04b8124fee
Dhruva_Gole <goledhruva@gmail.com>
2021-07-05 18:11:56 +0530

    PRU.cpp: removed double setvals

--- core/PRU.cpp
+++ core/PRU.cpp
@@ -100,7 +100,7 @@ public:
 	PruMemory(int pruNumber, InternalBelaContext* newContext) :
 		context(newContext)
 	{
-		prumanager.setVal(pruNumber,gRTAudioVerbose);
+		// prumanager.setVal(pruNumber,gRTAudioVerbose);
 		prumanager.map_pru_mem(PRUSS0_SHARED_DATARAM, (void **)&pruSharedRam);
 		audioIn.resize(context->audioInChannels * context->audioFrames);
 		audioOut.resize(context->audioOutChannels * context->audioFrames);
