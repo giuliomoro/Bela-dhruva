fdc4f9d55d2cd1b04893d811e2641e001fd0437b
Dhruva_Gole <goledhruva@gmail.com>
2021-07-11 19:07:45 +0530

    Syntax indentation improvements
    modified:   Makefile
    modified:   core/PRU.cpp

--- core/PRU.cpp
+++ core/PRU.cpp
@@ -226,12 +226,12 @@ PRU::PRU(InternalBelaContext *input_context)
   audio_expander_input_history(0), audio_expander_output_history(0),
   audio_expander_filter_coeff(0), pruUsesMcaspIrq(false), belaHw(BelaHw_NoHw)
 {
-	#ifdef ENABLE_PRU_UIO1
-        prumanager = new PruManagerUio(pru_number, gRTAudioVerbose);
-    #endif
-	#ifdef ENABLE_PRU_RPROC1
-		prumanager = new PruManagerRprocMmap(pru_number, gRTAudioVerbose);
-	#endif
+#ifdef ENABLE_PRU_UIO1
+	prumanager = new PruManagerUio(pru_number, gRTAudioVerbose);
+#endif
+#ifdef ENABLE_PRU_RPROC1
+	prumanager = new PruManagerRprocMmap(pru_number, gRTAudioVerbose);
+#endif
 }
 
 // Destructor
