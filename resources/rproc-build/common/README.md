d2c32918098664419e77adf5c70f5f940b08da19
Giulio Moro <giuliomoro@yahoo.it>
2021-07-29 22:50:38 +0000

    Makefile: build .out file via pasm+clpru when ENABLE_PRU_RPROC==1. Tidied up related and unrelated rproc-build files

--- resources/rproc-build/common/README.md
+++
@@ -1,2 +0,0 @@
-# common
-These are the files used by all the platforms for running the PRUs.
--- resources/rproc-build/common/init_pins_empty.h
+++
@@ -1,6 +0,0 @@
-// Defins an empty table to write_init_pins wont' complain
-
-#pragma DATA_SECTION(init_pins, ".init_pins")
-#pragma RETAIN(init_pins)
-const char init_pins[] =
-	"\0\0";
